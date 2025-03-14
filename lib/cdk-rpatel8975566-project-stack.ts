7eb203018dbbac391c53fb5bcbf54632a69b4c2d
Rahi Patel <hululu@Rahis-MacBook-Pro.local>
2025-03-14 13:14:46 -0400

    Initial commit

---
+++ lib/cdk-rpatel8975566-project-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkRpatel8975566ProjectStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkRpatel8975566ProjectQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
