5bd6d4c3408181732b985e00db5fc59c10b276ed
RahiPatel1172 <rpatel5566@conestogac.on.ca>
2025-03-22 14:24:18 -0400

    Update cdk-rpatel8975566-project.ts

--- bin/cdk-rpatel8975566-project.ts
+++ bin/cdk-rpatel8975566-project.ts
@@ -1,14 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { PipelineStack } from '../lib/pipeline-stack';
+import { CdkStack } from '../lib/lib/cdk-rpatel8975566-project-stack';
 
 const app = new cdk.App();
-new PipelineStack(app, 'rpatel-8975566-pipeline-stack', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: 'us-east-2'
-  }
-});
-
-app.synth();
+new CdkStack(app, 'rpatel-8975566-pipeline-stack', {});
